4a8b0f0d2e2c6c2dca369b9aa3755b4367a21cb4
OoiBP <bpooi0702@gmail.com>
2019-05-20 10:09:58 +0800

    Add Paper

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Paper, Button, Typography } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Paper>
+        <Typography align="center" variant="h1" gutterBottom>Learn Material UI</Typography>
+        <Button variant="contained" color="primary">Here is your button</Button>
+      </Paper>
+    )
+  }
 }
 
 export default App;
